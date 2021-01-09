34a2b0e4aea52c7b8356b038f35c8991e38fb967
Romulo S. Ramos <rsromulo@msn.com>
2021-01-08 22:08:26 -0300

    Add Routers and stepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+
+import './styles.css'
+import StepsHeder from './StepsHerder'
+
+function Order() {
+  return (
+    <div className="orders-container">
+      <StepsHeder />
+    </div> 
+  );
+}
+
+export default Order;
---
+++ front-web/src/Routes/index.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "../Home";
+import Navbar from "../Navbar";
+import Order from "../Orders";
+
+function Routes(){
+  return(
+    <BrowserRouter>
+      <Navbar/>
+      <Switch>
+        <Route path="/orders">
+          <Order />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+
+}
+ 
+export default Routes;
